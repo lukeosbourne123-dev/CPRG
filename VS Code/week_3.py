a19a368667408dadefa8b5c5bc5a5a2af0bed88c
luke osbourne <lukeosbourne123@outlook.com>
2025-09-18 15:46:17 -0600

    week 3

---
+++ VS Code/week_3.py
@@ -0,0 +1,20 @@
+# this is is our 3rd class
+'''
+multi-line comment 3 ' above and below
+'''
+
+# Review
+# Assignment 
+
+# write 4 assignment statements for int, float, boolean, string; print; print type
+
+x = 10+2
+y = 7.9
+q = True
+msg = "Hi i am a new CPRG student"
+
+print("the value of the variable x is: ",x,"and the type is ",type(x))
+print("the value of the variable y is: ",y,"and the type is ",type(y))
+print("the value of the variable q is: ",q,"and the type is ",type(q))
+print("the value of the variable msg is: ",msg,"and the type is ",type(msg))
+
